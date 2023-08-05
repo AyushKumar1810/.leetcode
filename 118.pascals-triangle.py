fa444b9762a0b576af4c991c2562951d671ad45b
Ayush <ayushsri1810@gmail.com>
2023-08-05 16:17:44 +0530

    s

---
+++ 118.pascals-triangle.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=118 lang=python3
+#
+# [118] Pascal's Triangle
+#
+
+# @lc code=start
+#NOTE: We can solve this Problem by using nCr where n will Be pascal traiangle size and r will be row no we have to find 
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+
+        
+# @lc code=end
+
---
+++ 119.pascals-triangle-ii.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=119 lang=python3
+#
+# [119] Pascal's Triangle II
+#
+
+# @lc code=start
+class Solution:
+    def getRow(self,rowIndex):
+        if rowIndex == 0:
+            return [1]
+
+        prev_row = [1]
+        for i in range(1, rowIndex + 1):
+            curr_row = [1]
+            for j in range(1, i):
+                curr_row.append(prev_row[j - 1] + prev_row[j])
+            curr_row.append(1)
+            prev_row = curr_row
+
+        return prev_row
+
+# # Example usage:
+# if __name__ == "__main__":
+#     k = 3
+#     row = getRow(k)
+#     print("Row", k, "of Pascal's Triangle:", row)
+# )
+
+
+# @lc code=end
+
