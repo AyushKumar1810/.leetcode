6637c1a9a1a325608996fa65257f454197d1ca2e
Ayush <ayushsri1810@gmail.com>
2023-08-07 00:00:24 +0530

    df

---
+++ 268.missing-number.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=268 lang=python3
+#
+# [268] Missing Number
+#
+
+# @lc code=start
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+
+    # Calculate the sum of numbers from 0 to n using the arithmetic formula (n*(n+1))/2
+        expected_sum = (n * (n + 1)) // 2
+
+    # Calculate the actual sum of the elements in the array
+        actual_sum = sum(nums)
+
+    # The missing number is the difference between the expected sum and the actual sum
+        return expected_sum - actual_sum
+    # def missingNumber(nums):
+        # # missing = len(nums)
+        # # for i, num in enumerate(nums):
+        # #     missing ^= i ^ num
+        # # return missing
+
+        
+# @lc code=end
+
