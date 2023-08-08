2c47141459da0457233bb7917bd850393661e783
Ayush <ayushsri1810@gmail.com>
2023-08-09 00:19:19 +0530

    se

---
+++ 1863.sum-of-all-subset-xor-totals.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1863 lang=python3
+#
+# [1863] Sum of All Subset XOR Totals
+#
+
+# @lc code=start
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        
+# @lc code=end
+
---
+++ 876.middle-of-the-linked-list.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=876 lang=python3
+#
+# [876] Middle of the Linked List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow,fast=head,head
+        while fast and fast.next:
+            slow , fast= slow.next , fast.next.next
+        return slow
+# @lc code=end
+
