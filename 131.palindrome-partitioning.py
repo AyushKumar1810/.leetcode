c78f9e0266746df2eb91651e15443bee3af2789d
Ayush <ayushsri1810@gmail.com>
2023-08-12 15:09:49 +0530

    aa

--- 131.palindrome-partitioning.py
+++ 131.palindrome-partitioning.py
@@ -7,6 +7,20 @@
 # @lc code=start
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
+        result=[]
+        def backtrack(start , current_partitions):
+            if start ==len(s):
+                result.append(current_partitions[:])
+                return
+            for end in range (start + 1 ,len(s)+1) :
+                if s[start:end] == s[start:end][::-1]:
+                    current_partitions.append(s[start:end])
+                    backtrack(end ,current_partitions)
+                    current_partitions.pop()
+        backtrack(0,[])
+        return result
+
+
         
 # @lc code=end
 
---
+++ 60.permutation-sequence.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=60 lang=python3
+#
+# [60] Permutation Sequence
+#
+
+# @lc code=start
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        
+# @lc code=end
+
