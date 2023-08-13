11ef16015d7ae31332db9785482ea4beb0aa8d43
Ayush <ayushsri1810@gmail.com>
2023-08-13 12:18:13 +0530

    s

--- 4.median-of-two-sorted-arrays.py
+++ 4.median-of-two-sorted-arrays.py
@@ -19,14 +19,14 @@
 
 
 # with use of inbuilt python function 
-class Solution:
-    def findMedianSortedArrays(self, nums1, nums2) -> float:
-        merged = sorted(nums1 + nums2)
-        length = len(merged)
-        if length % 2 == 0:
-            return (merged[length // 2 - 1] + merged[length // 2]) / 2
-        else:
-            return merged[length // 2]
+# class Solution:
+    # # def findMedianSortedArrays(self, nums1, nums2) -> float:
+    # #     merged = sorted(nums1 + nums2)
+    # #     length = len(merged)
+    # #     if length % 2 == 0:
+    # #         return (merged[length // 2 - 1] + merged[length // 2]) / 2
+    # #     else:
+    # #         return merged[length // 2]
 # Very complex solution :
 class Solution:
     def findMedianSortedArrays(self, nums1,nums2):
@@ -65,7 +65,7 @@ class Solution:
                 # Adjust the split point in array A to the right
                 left = splitA + 1
 
-
+    
         
 # @lc code=end
 
