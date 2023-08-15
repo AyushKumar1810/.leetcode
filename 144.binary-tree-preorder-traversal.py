712bff80baaf5412bde7ecaded5aa2c6d035fedf
Ayush <ayushsri1810@gmail.com>
2023-08-16 00:12:19 +0530

    ff

---
+++ 144.binary-tree-preorder-traversal.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=144 lang=python3
+#
+# [144] Binary Tree Preorder Traversal
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        
+        if not root:
+            return []
+        preorder=[]
+        stack=[root] # Storing root value 
+        while stack :
+            node = stack.pop() # taking out root and adding it's value to the result s it's preorder Root => left => right
+            preorder.append(node.val)
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return preorder
+        
+
+        
+        
+# @lc code=end
+
---
+++ 496.next-greater-element-i.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=496 lang=python3
+#
+# [496] Next Greater Element I
+#
+
+# @lc code=start
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack = []
+        next_greater = {}
+    
+    # Iterate through nums2 to find the next greater element for each element
+        for num in nums2:
+            while stack and num > stack[-1]:
+                next_greater[stack.pop()] = num
+            stack.append(num)
+    
+    # Find the next greater element for each element in nums1
+        result = []
+        for num in nums1:
+            result.append(next_greater.get(num, -1))
+    
+        return result
+
+# Example usage
+# nums1 = [4, 1, 2]
+# nums2 = [1, 3, 4, 2]
+# result = nextGreaterElement(nums1, nums2)
+# print(result)  # Output: [-1, 3, -1]
+
+        
+# @lc code=end
+
