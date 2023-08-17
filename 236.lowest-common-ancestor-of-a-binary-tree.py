2e31e18ea6df2d08829ec44bba02b1d0d681eb1d
Ayush <ayushsri1810@gmail.com>
2023-08-18 02:42:46 +0530

    se

--- 236.lowest-common-ancestor-of-a-binary-tree.py
+++ 236.lowest-common-ancestor-of-a-binary-tree.py
@@ -14,6 +14,24 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+# Base case: if root is None or one of the nodes is the root, return root
+
+        if root is None or root==p or root==q :
+            return root
+            # Recurse on the left subtree to find LCA for p and q
+
+        left= self.lowestCommonAncestor(root.left,p,q)
+        # Recurse on the right subtree to find LCA for p and q
+
+        right = self.lowestCommonAncestor(root.right,q,p)
+        # If one of the subtrees didn't find LCA, return the other
+
+        if left is None:
+            return right
+        elif right is None:
+            return left
+        else: # Both subtrees found LCA, so current root is the LCA
+            return root
         
 # @lc code=end
 
