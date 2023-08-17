b5e73d2e2a6197a0420eab4b5c9336b71dcc3ed0
Ayush <ayushsri1810@gmail.com>
2023-08-18 02:33:01 +0530

    ff

---
+++ 236.lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=236 lang=python3
+#
+# [236] Lowest Common Ancestor of a Binary Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        
+# @lc code=end
+
--- 543.diameter-of-binary-tree.py
+++ 543.diameter-of-binary-tree.py
@@ -13,16 +13,26 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        res=[0]
-        def dfs (root):
-            if not root:
-                return -1
-            left=dfs(root.left)
-            right=dfs(root.right)
-            res[0]=max(res[0], 2 + left + right)#for finfing diameter
-            return 1 + max(left,right) # for finding height we are just finifng max height of it's left or right node and just adding 1 
-        dfs(root)
-        return res[0]
+        # res=[0]
+        # def dfs (root):
+        #     if not root:
+        #         return -1
+        #     left=dfs(root.left)
+        #     right=dfs(root.right)
+        #     res[0]=max(res[0], 2 + left + right)#for finding diameter
+        #     return 1 + max(left,right) # for finding height we are just finding max height of it's left or right node and just adding 1 
+        # dfs(root)
+        # return res[0]
+        self.max_diameter=0
+        def depth(node):
+            if not node :
+                return 0
+            left_depth=depth(node.left)
+            right_depth=depth(node.right)
+            self.max_diameter=max(self.max_diameter , left_depth + right_depth)
+            return 1 + max(left_depth, right_depth)
+        depth(root)
+        return self.max_diameter
 
 # @lc code=end
 
