ea7cf6be6e0097315c6249886b5b4af2aa0902be
Ayush <ayushsri1810@gmail.com>
2023-08-21 01:27:43 +0530

    sad

--- 98.validate-binary-search-tree.py
+++ 98.validate-binary-search-tree.py
@@ -12,7 +12,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+    def isValidBST(self, root):
         def valid (node , left , right ):
             if not node:
                 return True
