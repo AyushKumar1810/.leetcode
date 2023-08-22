24b3c68d134c8aca22a77e1f45b69f5621fc0f8f
Ayush <ayushsri1810@gmail.com>
2023-08-23 02:04:28 +0530

    boy

--- 703.kth-largest-element-in-a-stream.py
+++ 703.kth-largest-element-in-a-stream.py
@@ -8,18 +8,35 @@
 import heapq
 
 class KthLargest:
-    def __init__(self, k:int, nums: list[int]):
-        self.nums = heapq.nlargest(k, nums)
-        self.k = k
-        heapq.heapify(self.nums)
-    
+
+    def __init__(self, k: int, nums: List[int]):
+        self.min_heap = []  # Min-heap to store the k largest elements
+        self.k = k  # The value of k
+
+        # Populate the min_heap with the initial k elements
+        for num in nums:
+            self.add(num)
+
     def add(self, val: int) -> int:
-        if len(self.nums) < self.k:
-            heapq.heappush(self.nums, val)
-        elif val > self.nums[0]:
-            heapq.heappushpop(self.nums, val)
-        
-        return self.nums[0]
+        # Push the value onto the min_heap
+        heapq.heappush(self.min_heap, val)
+
+        # If the size of min_heap exceeds k, pop the smallest element
+        if len(self.min_heap) > self.k:
+            heapq.heappop(self.min_heap)
+
+        # The top element of min_heap is the kth largest element
+        return self.min_heap[0]
+
+# Example usage
+# k = 3
+# nums = [4, 5, 8, 2]
+# kthLargest = KthLargest(k, nums)
+# print(kthLargest.add(3))  # Output: 4 (3rd largest element in [4, 5, 8, 2])
+# print(kthLargest.add(5))  # Output: 5 (3rd largest element in [5, 4, 8, 2])
+# print(kthLargest.add(10)) # Output: 5 (3rd largest element in [10, 5, 8, 2])
+# print(kthLargest.add(9))  # Output: 8 (3rd largest element in [9, 10, 8, 2])
+# print(kthLargest.add(4))  # Output: 8 (3rd largest element in [4, 9, 10, 8, 2])
 
         
 
---
+++ 733.flood-fill.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=733 lang=python3
+#
+# [733] Flood Fill
+#
+
+# @lc code=start
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        
+# @lc code=end
+
