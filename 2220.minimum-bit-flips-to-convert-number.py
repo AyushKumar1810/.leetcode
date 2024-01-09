2125ebf887ba08c18a25a823e592318484c0fe0b
Ayush <ayushsri1810@gmail.com>
2024-01-10 00:11:00 +0530

    ss

--- 2220.minimum-bit-flips-to-convert-number.py
+++ 2220.minimum-bit-flips-to-convert-number.py
@@ -7,6 +7,13 @@
 # @lc code=start
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
+        xor_resullt=start^goal
+        count = 0
+        while xor_resullt:
+            count+=xor_resullt & 1 
+            xor_resullt>>=1
+        return count   
+
         
 # @lc code=end
 
