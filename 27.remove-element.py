4cd003e269de98c6ea6e10c882271288040f4e08
Ayush <ayushsri1810@gmail.com>
2024-12-25 11:34:45 +0530

    commit

--- 27.remove-element.py
+++ 27.remove-element.py
@@ -5,12 +5,18 @@
 #
 
 # @lc code=start
+# class Solution:
+#     def removeElement(self, nums,val):
+#         for v in nums[:]:
+#             if v==val:
+#                 nums.remove(v)
+#         return len(nums)
 class Solution:
-    def removeElement(self, nums,val):
-        for v in nums[:]:
-            if v==val:
-                nums.remove(v)
-        return len(nums)
-        
+    def removeElement(self, nums, val):
+        while val in nums: # while val is in nums list 
+            nums.remove(val) # remove the first occurrence of val in nums list 
+        return len(nums) # return the length of nums list after removing all val in nums list 
+
+
 # @lc code=end
 
--- 35.search-insert-position.py
+++ 35.search-insert-position.py
@@ -6,18 +6,27 @@
 
 # @lc code=start
 #basically we are using binaray search as we have to make solution of O(log(n)).
+# class Solution:
+#     def searchInsert(self, nums , target ):
+#         l=0
+#         r=len(nums)-1
+#         while l<=r:
+#             mid=(l+r)//2
+#             if target==nums[mid]:
+#                 return mid
+#             if target > nums[mid]:
+#                 l=mid + 1
+#             else :
+#                 r=mid -1
+#         return l
 class Solution:
-    def searchInsert(self, nums , target ):
-        l=0
-        r=len(nums)-1
-        while l<=r:
-            mid=(l+r)//2
-            if target==nums[mid]:
-                return mid
-            if target > nums[mid]:
-                l=mid + 1
-            else :
-                r=mid -1
-        return l
+    def searchInsert(self, nums, target):
+        for i in range(len(nums)):  #iterating through the list 
+            if nums[i] == target: #if target is found in the list then return the index of that element   
+                return i 
+            if nums[i] > target: #if the element is greater than the target then return the index of that element 
+                return i 
+        return len(nums) #if the target is greater than all the elements in the list then return the length of the list 
+
 # @lc code=end
 
