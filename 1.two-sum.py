f37aa56d68edd3c43aab05ab620a80478c40deda
Ayush <ayushsri1810@gmail.com>
2024-12-25 10:48:11 +0530

    I am back

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -5,8 +5,23 @@
 #
 
 # @lc code=start
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for i in range(len(nums) - 1):
+#             for j in range(i+1, len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return [i, j]
+#More optimized solution 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_dict = {}
+        for i, num in enumerate(nums):
+            if target - num in num_dict:
+                return [num_dict[target - num], i]
+            num_dict[num] = i
+        return None
+
+
         
 # @lc code=end
 
